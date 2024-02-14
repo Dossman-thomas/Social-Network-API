62d3dbd025af873d72cb4b30f8fad62bfdff18b9
Tom Dossman <tdossman.development@gmail.com>
2024-02-14 15:51:33 -0500

    Post a new user configured

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,14 +42,28 @@ module.exports = {
       
       console.log(err);
       return res.status(500).json(err);
+
+    }
+
+  },
+
+  // POST a new user
+  async createUser(req,res) {
+
+    try {
+      
+      const user = await User.create(req.body);
+
+      res.json(user);
+
+    } catch (err) {
+      
+      console.log(err);
+      res.status(500).json(err);
       
     }
 
   }
-
-
-  // POST a new user
-
 
 
   // PUT to update a user by _id
