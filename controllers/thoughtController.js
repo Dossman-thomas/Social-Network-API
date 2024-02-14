df1dac39d76ea2901d9d3d3069eb53219e748f1f
Tom Dossman <tdossman.development@gmail.com>
2024-02-14 16:44:45 -0500

    createThought controller configured

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -47,10 +47,31 @@ async getSingleThought(req, res) {
 
 },
 
-
 // POST to create a new thought (push the created thought's _id to the associated user's thoughts array field)
+async createThought(req, res) {
+  try {
+    // Create a new thought
+    const thought = await Thought.create(req.body);
 
+    // Update the associated user's thoughts array field
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: thought.username },
+      { $push: { thoughts: thought._id } },
+      { new: true }
+    );
 
+    // If the associated user is not found, handle the case appropriately
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'Associated user not found' });
+    }
+
+    // Return the created thought
+    res.json(thought);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+},
 
 // PUT to update a thought by _id
 
