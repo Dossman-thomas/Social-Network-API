d3a261a0516486da42ae98eeaa88743e4d466724
Tom Dossman <tdossman.development@gmail.com>
2024-02-14 15:38:01 -0500

    Get all users controller function configured

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,34 +3,49 @@ const { User, Thought } = require('../models');
 
 module.exports = {
 
-// GET all users
+  // GET all users
+  async getUsers(req,res) {
+
+    try {
+
+      const users = await User.find();
+
+      res.json(users);
+      
+    } catch (err) {
+      
+      console.log(err);
+      return res.status(500).json(err);
+      
+    }
+
+  }
+
+
+  // GET single user by _id and populated thought and friend data
 
 
 
-// GET single user by _id and populated thought and friend data
+  // POST a new user
 
 
 
-// POST a new user
+  // PUT to update a user by _id
 
 
 
-// PUT to update a user by _id
+  // DELETE to remove user by _id
 
 
 
-// DELETE to remove user by _id
+  // [BONUS] remove a user's associated thoughts when deleted
+
+
+  // POST to add a new friend to a user's friend list
 
 
 
-// [BONUS] remove a user's associated thoughts when deleted
-
-
-// POST to add a new friend to a user's friend list
-
-
-
-// DELETE to remove a friend from a user's friend list
+  // DELETE to remove a friend from a user's friend list
 
 
 
