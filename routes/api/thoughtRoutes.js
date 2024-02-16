db7086864ad967d2f4ad842829f795a5ea4674bb
Tom Dossman <tdossman.development@gmail.com>
2024-02-16 13:26:42 -0500

    thoughtRoutes configured

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,3 +8,24 @@ const {
   createReaction,
   deleteReaction,
 } = require('../../controllers/thoughtController');
+
+// api/thoughts
+router.route('/')
+  .get(getThoughts)
+  .post(createThought);
+
+// api/thoughts/:thoughtId 
+router.route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+  .post(createReaction);
+
+// api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
+modeule.exports = router;
